5437e71aa7bf91baaf324f91b55e8375c3566210
Recai cingöz <recaicingz@gmail.com>
2020-12-08 10:03:27 +0300

    Update idispatch.go

--- idispatch.go
+++ idispatch.go
@@ -1,7 +1,3 @@
-// Copyright 2010-2012 The W32 Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package win32
 
 import (
